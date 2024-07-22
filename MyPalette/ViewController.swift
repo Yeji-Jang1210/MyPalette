607ff2923cdd8a502c03597a3f7d9d45ccf88c22
Yeji-Jang1210 <jyg8727@gmail.com>
2024-07-22 16:14:39 +0900

    Initial Commit

---
+++ MyPalette/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyPalette
+//
+//  Created by 장예지 on 7/22/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
